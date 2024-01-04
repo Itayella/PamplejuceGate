8dd92876fdc0fe6caa5d83040384748ecfd78e5f
Itaye <itay.ella@gmail.com>
2024-01-04 14:51:31 +0200

    Add Noterazation

--- packaging/Pkg/MakeInstaller.sh
+++ packaging/Pkg/MakeInstaller.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 clear
       
-NAME="LadyBug Reverb"
-NAME2="LadyBugReverb"
-NAME3="LadyBug_Reverb_Installer"
+NAME="Gate"
+NAME2="Gate"
+NAME3="Gate_Installer"
 
 echo "Creating installer for ${NAME}"
 
@@ -23,7 +23,7 @@ cd ../build-xcode
 rm -rf "/Library/Application Support/Avid/Audio/Plug-Ins/${NAME}.aaxplugin"
 xcodebuild -scheme "${NAME2}_AAX" -destination 'platform=macOS' -configuration Release
                                                                                                           
-/Applications/PACEAntiPiracy/Eden/Fusion/Current/bin/wraptool sign --verbose --account noamlev5 --wcguid 867CDD30-76F1-11EE-849E-005056928F3B --signid "Developer ID Application: David Dobreen (H7SQV279UN)" --in "/Library/Application Support/Avid/Audio/Plug-Ins/${NAME}.aaxplugin" --out "../Installer/AAX/${NAME}.aaxplugin" --autoinstall on
+#/Applications/PACEAntiPiracy/Eden/Fusion/Current/bin/wraptool sign --verbose --account noamlev5 --wcguid 867CDD30-76F1-11EE-849E-005056928F3B --signid "Developer ID Installer: Itay Ella (8V43W798AG)" --in "/Library/Application Support/Avid/Audio/Plug-Ins/${NAME}.aaxplugin" --out "../Installer/AAX/${NAME}.aaxplugin" --autoinstall on
 
 
 AU="/Users/mac/Library/Audio/Plug-Ins/Components/${NAME}.component"
@@ -41,26 +41,25 @@ cd ../Installer
 cp -r "${AU}" "./AU/"
 cp -r "${VST3}" "./VST3/"
  
-codesign --deep --force --verify --verbose --sign "Developer ID Application: David Dobreen (H7SQV279UN)" "AU/${NAME}.component/" --timestamp --options runtime
-codesign --deep --force --verify --verbose --sign "Developer ID Application: David Dobreen (H7SQV279UN)" "VST3/${NAME}.vst3" --timestamp --options runtime 
+codesign --deep --force --verify --verbose --sign "Developer ID Installer: Itay Ella (8V43W798AG)" "AU/${NAME}.component/" --timestamp --options runtime
+codesign --deep --force --verify --verbose --sign "Developer ID Installer: Itay Ella (8V43W798AG)" "VST3/${NAME}.vst3" --timestamp --options runtime 
 
 
-pkgbuild --identifier "com.safaripedals.${NAME2}.vst3.pkg" --root VST3 --install-location /Library/Audio/Plug-Ins/VST3 "com.safaripedals.${NAME2}.vst3.pkg"
-pkgbuild --identifier "com.safaripedals.${NAME2}.component.pkg" --root AU --install-location /Library/Audio/Plug-Ins/Components "com.safaripedals.${NAME2}.component.pkg"
-pkgbuild --identifier "com.safaripedals.${NAME2}.aax.pkg" --root AAX --install-location /Library/Application\ Support/Avid/Audio/Plug-Ins "./Signed/com.safaripedals.${NAME2}.aax.pkg"
+pkgbuild --identifier "com.ShizanaRecords.${NAME2}.vst3.pkg" --root VST3 --install-location /Library/Audio/Plug-Ins/VST3 "com.ShizanaRecords.${NAME2}.vst3.pkg"
+pkgbuild --identifier "com.ShizanaRecords.${NAME2}.component.pkg" --root AU --install-location /Library/Audio/Plug-Ins/Components "com.ShizanaRecords.${NAME2}.component.pkg"
 
-productsign --sign "H7SQV279UN" "com.safaripedals.${NAME2}.vst3.pkg" "signed/com.safaripedals.${NAME2}.vst3.pkg"
-productsign --sign "H7SQV279UN" "com.safaripedals.${NAME2}.component.pkg" "signed/com.safaripedals.${NAME2}.component.pkg" 
+productsign --sign "8V43W798AG" "com.ShizanaRecords.${NAME2}.vst3.pkg" "signed/com.ShizanaRecords.${NAME2}.vst3.pkg"
+productsign --sign "8V43W798AG" "com.ShizanaRecords.${NAME2}.component.pkg" "signed/com.ShizanaRecords.${NAME2}.component.pkg" 
  
-rm "com.safaripedals.${NAME2}.component.pkg"
-rm "com.safaripedals.${NAME2}.vst3.pkg"
+rm "com.ShizanaRecords.${NAME2}.component.pkg"
+rm "com.ShizanaRecords.${NAME2}.vst3.pkg"
 
 productbuild --distribution "./Signed/distribution.xml" --resources ./Signed/Res/ --package-path "./Signed/" "${NAME3}.pkg"
-productsign --sign "H7SQV279UN" "${NAME3}.pkg" "./Signed/${NAME3}.pkg"
+productsign --sign "8V43W798AG" "${NAME3}.pkg" "./Signed/${NAME3}.pkg"
 
 rm ${NAME3}.pkg
 
-xcrun notarytool submit "./Signed/${NAME3}.pkg" --apple-id "duvrin@hotmail.com" --password "ktyu-ikjx-fqfx-jzuf" --team-id "H7SQV279UN" --wait 
+xcrun notarytool submit "./Signed/${NAME3}.pkg" --apple-id "itay.ella@gmail.com" --password "ktyu-ikjx-fqfx-jzuf" --team-id "8V43W798AG" --wait 
 xcrun stapler staple "./Signed/${NAME3}.pkg"
 
 echo "...finished" 
